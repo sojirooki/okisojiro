e660700349b0b8415c1f094ec9f00e8c826e4ee5
sojirooki <sk24jirou@gmail.com>
2023-05-04 16:31:52 +0900

    Add ブログ投稿保存処理

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ use App\Http\Controllers\PostController;
 Route::get('/', [PostController::class, 'index']);
 route::get('/posts/create', [PostController::class,'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
+Route::post('/posts', [PostController::class, 'store']);
